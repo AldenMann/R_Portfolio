16ff60ff8a5240ed9f588d5c5b009e26b90e7b2e
AldenMann <37489669+AldenMann@users.noreply.github.com>
2022-05-14 17:14:23 -0700

    Update Starwars_RSQLite.R
    Added a query, and saved results as a vector.

--- StarwaRs_RSQLite_Adventures/Starwars_RSQLite.R
+++ StarwaRs_RSQLite_Adventures/Starwars_RSQLite.R
@@ -3,9 +3,9 @@ library(RSQLite)
 
 # Set up RSQLite database in memory:
 
-SW_db = dbConnect(RSQLite::SQLite(), ":memory:")
+SW_db = dbConnect(RSQLite::SQLite(), ":memory:") # can connect to outside databases as well. 
 
-# Prepare data: films, starships and vehicles product error because of c(""). 
+# Prepare data: films, starships and vehicles product error because of c(""), thus I have removed these columns. 
 data = starwars %>% 
   select(
     -films, -starships, -vehicles
@@ -18,6 +18,15 @@ actors = dbWriteTable(SW_db, "actors", data )
 dbListTables(SW_db)
 
 dbReadTable(SW_db, "actors")
-help("dbReadTable,SQLiteConnection,character-method")
+# help("dbReadTable,SQLiteConnection,character-method")
 
+
+# Can use SQL queries now, instead of using typical dplyr/tidyverse functions to explore data.
+# You can query in the terminal or assign the results to an object for later processing. 
+
+# Query for orange eyes:
+orange_eyes = dbSendQuery(SW_db, "SELECT * FROM actors WHERE eye_color = 'orange'" )
+
+# Shows results so that we can see it a table-like format:
+dbFetch(orange_eyes)
 
