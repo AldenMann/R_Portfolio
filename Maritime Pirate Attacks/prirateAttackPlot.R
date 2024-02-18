be320c4819463afe888a3be3340ea99ce495b227
AldenMann <37489669+AldenMann@users.noreply.github.com>
2024-02-17 19:16:30 -0800

    More data handling

--- Maritime Pirate Attacks/prirateAttackPlot.R
+++ Maritime Pirate Attacks/prirateAttackPlot.R
@@ -1,7 +1,10 @@
 PirateAttacks_df = data_pirates # separating this as working data from the orignal loaded data.
 
 pirates = PirateAttacks_df %>% 
-  mutate(date = ymd(data)) %>% 
-  mutate() 
+  mutate(date = ymd(date)) %>% # might try an imputation for missing dates. 
+  mutate(time = hm(time)) # some values are NA so we'll need to impute for time.
 
 # need to review how to handle the NA values, I would like to use a little imputation as possible. 
+
+
+str(PirateAttacks_df)
