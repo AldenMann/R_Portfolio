2a67c11ea15fef2bbb38929037ab18f9e19e573b
AldenMann <37489669+AldenMann@users.noreply.github.com>
2024-02-25 18:00:14 -0800

    Working with time series data
    Time series manipulation on the maritime data. Kind of dissapointed that most of the 'time' data is missing. Might move forward without time data.

--- Maritime Pirate Attacks/prirateAttackPlot.R
+++ Maritime Pirate Attacks/prirateAttackPlot.R
@@ -1,10 +1,12 @@
-PirateAttacks_df = data_pirates # separating this as working data from the orignal loaded data.
+PirateAttacks_df = data_pirates # separating this as working data from the original loaded data.
 
-pirates = PirateAttacks_df %>% 
-  mutate(date = ymd(date)) %>% # might try an imputation for missing dates. 
-  mutate(time = hm(time)) # some values are NA so we'll need to impute for time.
+pirates = PirateAttacks_df 
+  
+  
+pirate_hm_impute = pirates %>% # get median time of day data to impute
+  filter(!is.na(time)) %>% # only values with time data
+  mutate(time = hm(time)) # convert to hour minutes with lubridate. Next step to is get median. 
 
-# need to review how to handle the NA values, I would like to use a little imputation as possible. 
-
-
-str(PirateAttacks_df)
+pirate_hm_median = pirate_hm_impute %>% 
+  select(time) %>% # selecting only the time column. 
+  # need to get median time in hours and minutes
