d419b9128de66ecf4b3a94fb266593c00d3577b7
AldenMann <37489669+AldenMann@users.noreply.github.com>
2024-07-10 19:53:49 -0700

    Update Finviz_data_fun.R
    Graph clean up

--- StockAnalysis_FinViz/Finviz_data_fun.R
+++ StockAnalysis_FinViz/Finviz_data_fun.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readxl)
+library(plotly)
 
 data = read_excel(choose.files())
 
@@ -26,6 +27,26 @@ G_series2 = ggplot(T_series2, aes(X = Sector, Y = `Sum Total Shares`)) +
   geom_col(aes(x = Sector, y = `Sum Total Shares`))
 
 G_series2.1 = ggplot(T_series2.1) +
-  geom_col(aes(x = Sector, y = `Total Tickers`))
+  geom_col(aes(x = Sector, y = `Total Tickers`,
+               color = Sector)) + 
+  labs(title = "Total Tickers by Sector",
+       subtitle = "Finviz Data") + 
+  theme(axis.text.x = element_text(angle = 35, hjust = 1)) + 
+  scale_y_continuous(breaks = seq(50,2000, by = 250))
 
 
+ggplotly(G_series2.1)
+
+# ---- Stock performance by dividend yield --- 
+# Does a stock performance correlate with it's dividend? More specifically, does a higher dividend have an inverse relationship on performance. 
+  
+# We can likely achieve this by using the EPS. Scatter plot 
+
+Q1_Data = data %>% filter(
+  `Market Cap` != is.na(`Market Cap`),
+  `Market Cap` > 50,
+  `Volume` > 200000,
+  `P/S` > 7
+)
+
+#Need to reread the question, to finish filtering. 
